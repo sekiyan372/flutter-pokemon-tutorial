813601458bfc9b3b641b2f949c70592eca7cdc42
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2023-05-20 22:24:09 +0900

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# myapp
+
+My flutter repository for practice.
+
+Flutter Tutorial by (ポケモンから学ぶFlutter)[https://zenn.dev/sugitlab/books/flutter_poke_app_handson]
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Sample App'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Hello World',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
