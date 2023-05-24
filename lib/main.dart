4e6370d30a6fd1bc2ede7e80a70ffb698d742bd5
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2023-05-24 09:06:47 +0900

    :sparkles: step3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:js';
+
+import './poke_detail.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,55 +19,55 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Sample App'),
+      home: const TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(32),
-                child: Image.network(
+        body: ListView.builder(
+      itemCount: 10000,
+      itemBuilder: ((context, index) => PokeListItem(index: index)),
+    ));
+  }
+}
+
+class PokeListItem extends StatelessWidget {
+  const PokeListItem({Key? key, required this.index}) : super(key: key);
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+            color: Colors.yellow.withOpacity(.5),
+            borderRadius: BorderRadius.circular(10),
+            image: const DecorationImage(
+                fit: BoxFit.fitWidth,
+                image: NetworkImage(
                   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-                  height: 100,
-                  width: 100,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  'No.25',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
+                ))),
+      ),
+      title: const Text(
+        'pikachu',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: const Text('⚡️electric'),
+      trailing: const Icon(Icons.navigate_next),
+      onTap: () => {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) => const PokeDetail(),
           ),
-          const Text(
-            'pikachu',
-            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-          ),
-          const Chip(
-            label: Text('electric'),
-            backgroundColor: Colors.yellow,
-          ),
-        ],
-      )),
+        ),
+      },
     );
   }
 }
